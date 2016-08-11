6373f2df19f1e99391423943377f7b595a8937da
Michael Kaminsky <mkaminsky11@gmail.com>
2016-08-11 13:03:29 -0400

    fix ansi coloring

--- ascii/colorize.py
+++ ascii/colorize.py
@@ -12,7 +12,7 @@ def colorize(rawChar, px):
 	return color + rawChar + reset
 
 def wrapAnsi16(code, offset):
-	return '\u001b[' + str(code + offset) + 'm'
+	return '\x1b[38;5;' + str(code + offset) + 'm'
 
 def wrapAnsi256(code, offset):
-	return '\u001b[' + str(38 + offset) + ';5;' + str(code) + 'm'
+	return '\x1b[38;5;' + str(38 + offset) + ';5;' + str(code) + 'm'
